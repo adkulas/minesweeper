d964e303df817a1be481c20d0ff31f05543e9416
Adam Kulas <adam.kulas1@gmail.com>
2020-03-02 16:35:21 -0500

    grid generated

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,79 @@ import './Board.css';
 import Cell from './Cell';
 
 export class Board extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            temp: "Test",
+            grid: this.createGrid(5)
+        }
+    }
+    
+    createGrid = (size) => {
+        
+        // initialize matrix
+        let matrix = [];
+        for(let i=0; i < size; i++) {
+            matrix[i] = [];
+            for(let j=0; j < size; j++) {
+                matrix[i][j] = "";
+            }
+        }
+
+        // place mines
+        let numMines = size;
+        while (numMines > 0) {
+            let r = Math.floor(Math.random()*size);
+            let c = Math.floor(Math.random()*size);
+            
+            if (matrix[r][c] === "") {
+                matrix[r][c] = "💣";
+                numMines--;
+            }
+        }
+        
+        // label cells
+
+        // loops through cells, at each cell count number of bombs around
+        for(let i=0; i < matrix.length; i++) {
+            for(let j=0; j < matrix.length; j++) {
+                
+                if (matrix[i][j] == "💣") {
+                    continue;
+                }
+
+                // count all bombs around cell
+                let count = 0;
+                for(let r=i-1; r<=i+1; r++) {
+                    if (r<0 || r>=size) {
+                        continue;
+                    }
+                    
+                    for(let c=j-1; c<=j+1; j++) {
+                        if (c<0 || c>size) {
+                            continue;
+                        }
+
+                        if (matrix[r][c] === "💣") {
+                            count++;
+                        }
+                    }
+
+                }
+                matrix[i][j] = count;
+            }
+        }
+
+
+        return matrix
+    }
+
     render() {
+        
+        
+        const cellsFlat = [].concat(...this.state.grid);   
+        const cells = cellsFlat.map( item => <Cell number={item} /> )
+
         return (
             <div className="Board">
                 
@@ -13,17 +85,12 @@ export class Board extends React.Component {
                  */}
 
                 {/* <Cell /> */}
-                
+                 
+                <p>{this.state.grid.length}</p>
+                <p>{cellsFlat.length}</p>
+                <p>{Math.random()}</p>
                 <div className="Grid">
-                    <Cell number={1} />
-                    <Cell number={2} />
-                    <Cell number={3} />
-                    <Cell number={4} />
-                    <Cell number={5} />
-                    <Cell number={6} />
-                    <Cell number={7} />
-                    <Cell number={8} />
-                    <Cell number={9} />
+                        {cells}
                 </div>
             </div>
         )
