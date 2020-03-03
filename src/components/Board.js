2c478d7234f44d6407b4e83f28e12804fde5ceda
Adam Kulas <adam.kulas1@gmail.com>
2020-03-03 16:33:50 -0500

    click handler and grid generate changes

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,112 +1,122 @@
-import React from 'react';
-import './Board.css';
-import Cell from './Cell';
+import React from 'react'
+import './Board.css'
+import Cell from './Cell'
 
 export class Board extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            grid: this.createGrid(5),
-            visible: this.createVisGrid(5),
-
-        }
-    }
-    
-    createVisGrid = (size) => {
-        
-        // initialize matrix
-        let matrix = [];
-        for(let i=0; i < size; i++) {
-            matrix[i] = [];
-            for(let j=0; j < size; j++) {
-                matrix[i][j] = false;
-            }
+            // grid: [],
+            grid: this._initializeGrid(5),
         }
     }
 
-    createGrid = (size) => {
-        
-        // initialize matrix
-        let matrix = [];
-        for(let i=0; i < size; i++) {
-            matrix[i] = [];
-            for(let j=0; j < size; j++) {
-                matrix[i][j] = "";
+    _initializeGrid = size => {
+        const bombLocations = this._initBombs(size, size, size)
+
+        // initialize grid
+        let grid = []
+        for (let i = 0; i < size; i++) {
+            grid[i] = []
+            for (let j = 0; j < size; j++) {
+                grid[i][j] = {
+                    val: '',
+                    isBomb: false,
+                    row: i,
+                    col: j,
+                    visible: true,
+                    flagged: false,
+                    bombCount: 0,
+                }
+
+                if (bombLocations.has([i, j].toString())) {
+                    grid[i][j].isBomb = true
+                    grid[i][j].val = '💣'
+                }
             }
         }
+        this._initCounts(grid)
+        return grid
+    }
 
+    _initBombs = (numMines, numRows, numCols) => {
         // place mines
-        let numMines = size;
-        while (numMines > 0) {
-            let r = Math.floor(Math.random()*size);
-            let c = Math.floor(Math.random()*size);
-            
-            if (matrix[r][c] === "") {
-                matrix[r][c] = "💣";
-                numMines--;
+        const bombLocations = new Set()
+        while (bombLocations.size < numMines) {
+            let r = Math.floor(Math.random() * numRows)
+            let c = Math.floor(Math.random() * numCols)
+
+            if (!bombLocations.has([r, c].toString())) {
+                bombLocations.add([r, c].toString())
             }
         }
-        
-        // label cells
 
+        return bombLocations
+    }
+
+    _initCounts = matrix => {
         // loops through cells, at each cell count number of bombs around
-        for(let i=0; i < matrix.length; i++) {
-            for(let j=0; j < matrix.length; j++) {
-                
-                if (matrix[i][j] == "💣") {
-                    continue;
+        let size = matrix.length
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix.length; j++) {
+                if (matrix[i][j].isBomb) {
+                    continue
                 }
 
                 // count all bombs around cell
-                let count = 0;
-                for(let r=0; r<3; r++) {
-                    if (i-1+r < 0 || i-1+r >= size) {
-                        continue;
+                let count = 0
+                for (let r = 0; r < 3; r++) {
+                    if (i - 1 + r < 0 || i - 1 + r >= size) {
+                        continue
                     }
-                    
-                    for(let c=0; c<3; c++) {
-                        if (j-1+c < 0 || j-1+c >= size ) {
-                            continue;
+
+                    for (let c = 0; c < 3; c++) {
+                        if (j - 1 + c < 0 || j - 1 + c >= size) {
+                            continue
                         }
 
-                        if (matrix[i-1+r][j-1+c] === "💣") {
-                            count++;
+                        if (matrix[i - 1 + r][j - 1 + c].isBomb) {
+                            count++
                         }
                     }
                 }
-                matrix[i][j] = count.toString();
+                matrix[i][j].bombCount = count
+                matrix[i][j].val = count.toString()
             }
         }
-
-        return matrix
     }
 
-    reveal = (i, j) => {
-        this.setState({grid: this.state.grid})
-    } 
+    handleClick = (row, col) => {
+        // alert('Hello')
+        console.log(row, col)
+        this.setState({
+            grid: this.state.grid.map((row, i) =>
+                row.map((cell, j) => {
+                    const result = { ...cell }
+                    if (row === i && col === j) {
+                        result.visible = false
+                    }
+                    console.log(i, j)
+                    return result
+                })
+            ),
+        })
+    }
 
     render() {
-        
-        
-        const cellsFlat = [].concat(...this.state.grid);   
-        const cells = cellsFlat.map( item => <Cell {...this.state} /> );
+        const cellsFlat = [].concat(...this.state.grid)
+        const cells = cellsFlat.map((item, i) => (
+            <Cell key={i} {...item} handleClick={this.handleClick} />
+        ))
 
         return (
             <div className="Board">
-                
                 {/* 
-                <Controls />
-                <Grid />
-                 */}
-
+                                        <Controls />
+                                        <Grid />
+                                         */}
                 {/* <Cell /> */}
-                 
-                <p>{this.state.grid.length}</p>
-                <p>{cellsFlat.length}</p>
-                <p>{Math.random()}</p>
-                <div className="Grid">
-                        {cells}
-                </div>
+                <div className="Grid"> {cells} </div>{' '}
             </div>
         )
     }
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
-import './Cell.css';
+import './Cell.css'
 
 export class Cell extends Component {
     constructor(props) {
-        super(props);
-
-
+        super(props)
     }
-    
+
+    _handleClick = event => {
+        this.props.handleClick(this.props.row, this.props.col)
+    }
+
     render() {
         return (
-            <button className="Cell">
-                {this.props.grid[0][0]}
+            <button className="Cell" onClick={this._handleClick}>
+                {this.props.visible ? this.props.val : ''}
             </button>
         )
     }
