d548944a136eea5abd7d76b1e2289fb3c9017901
Adam Kulas <adam.kulas1@gmail.com>
2020-03-09 13:57:00 -0400

    favicon and index html updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Mine Sweeper</h1>
+                <h1>Minesweeper</h1>
             </header>
             <Board />
         </div>
