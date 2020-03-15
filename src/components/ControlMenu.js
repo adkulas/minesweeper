8c89f0859ec379038bf3383475364cf8edf26bc0
Adam Kulas <adam.kulas1@gmail.com>
2020-03-14 23:31:58 -0400

    material ui buttons added

--- src/components/ControlMenu.js
+++ src/components/ControlMenu.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import './ControlMenu.css'
 import Timer from './Timer'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import Button from '@material-ui/core/Button'
+import AlarmIcon from '@material-ui/icons/Alarm'
+import FlagTwoToneIcon from '@material-ui/icons/FlagTwoTone'
 
 export class ControlMenu extends Component {
     _handleMode = event => {
-        this.props.handleDifficultyChange(event.target.value)
+        this.props.handleDifficultyChange(event.currentTarget.value)
         this.props.handleReset()
         return
     }
@@ -13,46 +17,51 @@ export class ControlMenu extends Component {
         return (
             <ul className="Container">
                 <li>
-                    <button
-                        className="button"
-                        value="Easy"
-                        onClick={this._handleMode}
+                    <ButtonGroup
+                        variant="contained"
+                        color="primary"
+                        aria-label="contained primary button group"
                     >
-                        Easy
-                    </button>
-
-                    <button
-                        className="button"
-                        value="Medium"
-                        onClick={this._handleMode}
-                    >
-                        Medium
-                    </button>
-                    <button
-                        className="button"
-                        value="Hard"
-                        onClick={this._handleMode}
-                    >
-                        Hard
-                    </button>
+                        <Button value="Easy" onClick={this._handleMode}>
+                            Easy
+                        </Button>
+                        <Button value="Medium" onClick={this._handleMode}>
+                            Medium
+                        </Button>
+                        <Button value="Hard" onClick={this._handleMode}>
+                            Hard
+                        </Button>
+                    </ButtonGroup>
                 </li>
                 <li>
                     <div className="Container middle">
-                        <span>
-                            '🚩️' {this.props.bombCount - this.props.flagCount}
-                        </span>
-                        <span>
+                        <div className="Container">
+                            <FlagTwoToneIcon
+                                color="secondary"
+                                style={{ fontSize: '4rem' }}
+                            />{' '}
+                            {this.props.bombCount - this.props.flagCount}
+                        </div>
+                        <div className="Container">
+                            <AlarmIcon
+                                color="default"
+                                style={{ fontSize: '4rem' }}
+                            />
                             <Timer
                                 gameStarted={this.props.gameStarted}
                                 gameOver={this.props.gameOver}
                             />
-                        </span>
+                        </div>
                     </div>
                 </li>
                 <li>
-                    <button className="button" onClick={this.props.handleReset}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={this.props.handleReset}
+                    >
                         Reset
-                    </button>
+                    </Button>
                 </li>
             </ul>
         )
